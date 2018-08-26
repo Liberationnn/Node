b796c4202f670082e96295811e905f90b97d2ee4
SR <183428323@qq.com>
2018-08-26 14:01:23 +0800

    func

--- event/func.js
+++ event/func.js
@@ -10,3 +10,20 @@ let newSay = (word) => {
 newSay('我爱你');
 newSay('i love you');
 
+
+let eat = (times, callback) => {
+    let i = 0;
+    return () => {
+        if (++i === times) {
+            callback();
+        }
+    };
+};
+let newEat = eat(5, () => {
+    console.log('我吃完了');
+});
+newEat();
+newEat();
+newEat();
+newEat();
+newEat(); //只有第五次才有输出
