1bccea698706fced95b6e843424e479001466be7
sunrui <183428323@qq.com>
2018-08-27 17:27:01 +0800

    trans

---
+++ buffer/3.trans.js
@@ -0,0 +1,9 @@
+// 二、八、十六进制转换成十进制
+console.log(parseInt('11', 2)); //3
+console.log(parseInt('11', 8)); //9
+console.log(parseInt('11', 16)); //17
+
+// 十进制转换成二、八、十六进制
+console.log((3).toString(2)); //11
+console.log((9).toString(8)); //11
+console.log((17).toString(16)); //11
