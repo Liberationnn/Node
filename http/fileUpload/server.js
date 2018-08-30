622c74ac8fc6a5e6fd1deb5854a8cecb8349f366
sunrui <183428323@qq.com>
2018-08-30 20:49:45 +0800

    fileUpload

---
+++ http/fileUpload/server.js
@@ -0,0 +1,46 @@
+let fs = require('fs'),
+    http = require('http'),
+    MimeLookup = require('mime-lookup'),
+    mime = new MimeLookup(require('mime-db')),
+    util = require('util'),
+    url = require('url'),
+    querystring = require('querystring'),
+    formidable = require('formidable');
+
+let count = 0;
+
+let server = http.createServer((req, res) => {
+    let urlObj = url.parse(req.url);
+    let {pathname} = urlObj;
+
+    if (pathname === '/') {
+        fs.createReadStream('./zone.html').pipe(res);
+    } else if (pathname === '/fileUpload') {
+        let parser = new formidable();
+        parser.parse(req, (err, fields, files) => {
+            let {fileName, type, total, size, index} = fields,
+                src = fs.createReadStream(files.data.path),
+                tar = fs.createWriteStream(`${fileName}_${index}`);
+            tar.on('close', () => {
+                if (++count === parseInt(total)) {
+                    let fd = fs.openSync(`${fileName}.${type}`, 'a'),
+                        files = fs.readdirSync('./');
+                    files = files.filter((file) => file.startsWith(`${fileName}_`));
+                    files.sort((a, b) => a.split('_')[1] - b.split('_')[1]);
+                    files.forEach((file) => {
+                        let buffer = fs.readFileSync(file);
+                        fs.writeSync(fd, buffer, 0, buffer.length);
+                        fs.unlinkSync(file);
+                    });
+                    fs.closeSync(fd);
+                    count = 0;
+                }
+            });
+            src.pipe(tar);
+            res.end('ok');
+        });
+    } else {
+        res.end('404');
+    }
+
+}).listen(8080);
