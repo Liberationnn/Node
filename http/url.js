37a4dd84b4c2485d7c9acb6a101efeea0f9ad3ca
sunrui <183428323@qq.com>
2018-08-31 11:19:13 +0800

    url

--- http/url.js
+++ http/url.js
@@ -2,4 +2,4 @@ let url = require('url');
 
 let urlObj = url.parse('http://name:password@srrrrr:2333/path/sub?name=sr&age=22#top');
 console.log(urlObj);
-console.log(url.format(urlObj));
+// console.log(url.format(urlObj));
