1e85c7dedee52538421e5ff004d3e78a7212f2b1
sunrui <183428323@qq.com>
2018-09-02 14:18:05 +0800

    Etag

--- express-ext/6.etag.js
+++ express-ext/6.etag.js
@@ -3,28 +3,21 @@ let express = require('express'),
     http = require('http'),
     crypto = require('crypto');
 
-// 未完成版
 function getHash(str) {
     let shasum = crypto.createHash('sha1');
     return shasum.update(str).digest('hex');
 }
 
 function send(fileName, req, res) {
-    // 转换时间戳
-    function trans(time) {
-        return time.toString().slice(0, 10);
+    let ifNoneMatch = req.headers['if-none-match']; //取得最后修改时间
+    let data = fs.readFileSync(fileName);
+    if (ifNoneMatch === getHash(data)) {
+        res.statusCode = 304;
+        res.end();
+    } else {
+        res.writeHead(200, {'Etag': getHash(data)});
+        fs.createReadStream(fileName).pipe(res);
     }
-
-    let lastModified = new Date(req.headers['if-modified-since']); //取得最后修改时间
-    fs.stat(fileName, (err, stats) => {
-        if (trans(stats.mtime.getTime()) === trans(lastModified.getTime())) {
-            res.statusCode = 304;
-            res.end();
-        } else {
-            res.writeHead(200, {'Last-Modified': stats.mtime.toUTCString()});
-            fs.createReadStream(fileName).pipe(res);
-        }
-    });
 }
 
 http.createServer((req, res) => {
